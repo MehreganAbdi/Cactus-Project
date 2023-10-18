bf71e60e3b56bca06e1f2f3726e296121934f7a1
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-18 10:34:34 +0330

    Add project files.

---
+++ Cactus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cactus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
