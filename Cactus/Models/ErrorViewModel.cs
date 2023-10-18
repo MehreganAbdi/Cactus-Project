390165fa13505a9faa222717608109b0fac1bc8b
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-18 10:42:49 +0330

    Basics

--- Cactus/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cactus.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ CactusDomain/Data/Enums/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusDomain.Data.Enums
+{
+    public enum Brand
+    {
+        Nike , 
+        Adidas , 
+        Asics , 
+        Sacony ,
+        Brooks , 
+        NikeJordan , 
+        NikeTravis , 
+        Other
+    }
+}
