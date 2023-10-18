863c136f3d5618ab212f5dcbe4126c25436afbf3
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-18 11:22:51 +0330

    Db Added

---
+++ CactusDomain/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CactusDomain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CactusDomain.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<SizeAndCount> SizeAndCounts{ get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserCart> UsersCarts { get; set; }
+        public DbSet<UserFavorite> UserFavorites { get; set; }
+
+
+    }
+}
