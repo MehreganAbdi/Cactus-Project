5ac65502894a52df372acf46b5b70513814ef7f7
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-19 19:24:05 +0330

    Readt To Add Product Service

---
+++ Cactus/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using CactusApplication.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cactus.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await productRepository.GetAllProductsAsync();
+            if (products == null)
+            {
+                TempData["Error"] = "No Products Yet!";
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Cactus/ViewModels/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿using CactusDomain.Data.Enums;
+
+namespace Cactus.ViewModels
+{
+    public class ProductVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Cost { get; set; }
+        public Brand Brand { get; set; }
+        public string? AdditionalInfo { get; set; }
+    }
+}
