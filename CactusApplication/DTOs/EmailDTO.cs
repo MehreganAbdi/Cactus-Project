c226c4e5e60e3e2211d947d1482dc97457b0eb36
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-21 06:56:44 +0330

    Email DTOs Added

---
+++ CactusApplication/DTOs/EmailDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusApplication.DTOs
+{
+    public class EmailDTO
+    {
+        public string Subject { get; set; } = "No Subject";
+        public string Reciever { get; set; }
+        public string message { get; set; } = "No Message";
+    }
+}
---
+++ CactusApplication/DTOs/EmailVerificationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusApplication.DTOs
+{
+    public class EmailVerificationDTO
+    {
+        public string UserId { get; set; }
+        public string? Code { get; set; }
+    }
+}
