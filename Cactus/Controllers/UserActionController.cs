6327c18b8bb630f6af47777a0675b4ff1c92c539
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-22 17:29:00 +0330

    Ready To UserAction Methods To the Controlelr

---
+++ Cactus/Controllers/UserActionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cactus.Controllers
+{
+    public class UserActionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
