15b0b1a6e014ba359289978bb0571baeff19d858
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-23 17:43:08 +0330

    Cart Service Interface

---
+++ CactusApplication/DTOs/CartDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusApplication.DTOs
+{
+    public class CartDTO
+    {
+        public int CartItemId { get; set; }
+ 
+        public int ProductId { get; set; }
+ 
+        public string UserId { get; set; }
+        public ProductDetailDTO? Product { get; set; }
+    }
+}
---
+++ CactusApplication/Service/CartService.cs
@@ -0,0 +1,71 @@
+﻿using CactusApplication.DTOs;
+using CactusApplication.IService;
+using CactusDomain.IRepository;
+using CactusDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusApplication.Service
+{
+    public class CartService : ICartService
+    {
+        private readonly ICartRepository cartRepository;
+
+        public CartService(ICartRepository cartRepository)
+        {
+            this.cartRepository = cartRepository;
+        }
+        public bool AddToCart(CartDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddToCartAsync(CartDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartDTO ChangeToDTO(UserCart userCart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDTO> ChangeToDTOAsync(UserCart userCart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserCart ChangeToModel(CartDTO cartDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserCart> ChangeToModelAsync(CartDTO cartDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CartDTO> GetAllByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CartDTO>> GetAllByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFromCart(CartDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCartAsync(CartDTO item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
