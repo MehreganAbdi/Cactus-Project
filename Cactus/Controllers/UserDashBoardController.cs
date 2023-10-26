1da62fb4865f024f4f54412fa196dcfd3de3b563
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-26 17:25:35 +0330

    userdash repo added

---
+++ Cactus/Controllers/UserDashBoardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cactus.Controllers
+{
+    public class UserDashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CactusDomain/IRepository/IUserDashBoard.cs
@@ -0,0 +1,18 @@
+﻿using CactusDomain.Models;
+
+namespace CactusDomain.IRepository
+{
+    public interface IUserDashBoard
+    {
+        IEnumerable<UserCart> GetAll(string userId);
+        Task<IEnumerable<UserCart>> GetAllAsync(string userId);
+        Task<UserCart> GetByIdAsync(string userId);
+        UserCart GetById(string userId);
+        bool Save();
+        Task<bool> SaveAsync();
+        Task<bool> DeleteByIdAsync(string userId);
+        bool DeleteById(string userId);
+        bool Update(UserCart userCart);
+        Task<bool> UpdateAsync(UserCart userCart);
+    }
+}
