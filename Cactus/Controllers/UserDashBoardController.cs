c0e01c390f1f309c92e8021065f6adeb00297873
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-27 11:19:05 +0330

    ...

--- Cactus/Controllers/UserDashBoardController.cs
+++ Cactus/Controllers/UserDashBoardController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CactusApplication.IService;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cactus.Controllers
 {
     public class UserDashBoardController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserDashBoardService userDashBoardService;
+
+        public UserDashBoardController(IUserDashBoardService userDashBoardService)
         {
-            return View();
+            this.userDashBoardService = userDashBoardService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var userCarts = await userDashBoardService.GetAllByUserIdAsync(User.Identity.GetUserId());
+
+            return View(userCarts);
+        }
+
+
+        public async Task<IActionResult> DeleteItem(int Id)
+        {
+            await userDashBoardService.RemoveItemAsync(Id);
+            return RedirectToAction("Index", "UserDashBoard");
+        }
+
 
 
     }
