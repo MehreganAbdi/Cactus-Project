4a5d8cbf15d81409a580c43bcda38f19d4dd2259
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-30 09:32:50 +0330

    IRepo Added Initialy

---
+++ CactusDomain/IRepository/IAdminDashBoard.cs
@@ -0,0 +1,26 @@
+﻿using CactusDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusDomain.IRepository
+{
+    public interface IAdminDashBoard
+    {
+        IEnumerable<User> GetAllUsers();
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        IEnumerable<UserCart> GetUserCartsByUserId(string userId);
+        Task<IEnumerable<UserCart>> GetUserCartsByUserIdAsync(string userId);
+        IEnumerable<Product> GetAllProducts();
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        User GetUserByUserId(string userId);
+        Task<User> GetUserByUserIdAsync(string userId);
+        IEnumerable<Product> GetSoldOutProducts();
+        Task<IEnumerable<Product>> GetSoldOutProductsAsync();
+
+    }
+}
+
+
