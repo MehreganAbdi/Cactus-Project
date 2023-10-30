566e5f5acdde0babc0da8267780491aea04c7c3a
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-30 10:00:00 +0330

    IService Added

---
+++ CactusApplication/IService/IAdminDashBoardService.cs
@@ -0,0 +1,35 @@
+﻿using CactusApplication.DTOs;
+using CactusDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CactusApplication.IService
+{
+    public interface IAdminDashBoardService
+    {
+        ProductDTO ChangeProductToProductDTO(Product product);
+        Task<ProductDTO> ChangeProductToProductDTOAsync(Product product);
+        Product ChangeProductDTOToProduct(ProductDTO productDTO);
+        Task<Product> ChangeProductDTOToProductAsync(ProductDTO productDTO);
+
+        UserDTO ChangeUserToUserDTO(User user);
+        Task<UserDTO> ChangeUserToUserDTOAsync(User user);
+        User ChangeUserDTOToUser(UserDTO userDTO);
+        Task<User> ChangeUserDTOToUserAsync(UserDTO userDTO);
+
+        IEnumerable<UserDTO> GetAllUsers();
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+        IEnumerable<ProductDTO> GetAllProducts();
+        Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+        IEnumerable<ProductDTO> GetSoldOutProducts();
+        Task<IEnumerable<ProductDTO>> GetSoldOutProductsAsync();
+        UserDTO GetUserById(string userId);
+        Task<UserDTO> GetUserByIdAsync(string userId);
+        IEnumerable<CartDTO> GetAllUserCartsByUserId(string userId);
+        Task<IEnumerable<CartDTO>> GetAllUserCartsByUserIdAsync(string userId);
+
+    }
+}
