0d6ebfaf95ffe073f24d31b235f929a34776d794
Mehregan Abdi <mehreganabdix@gmail.com>
2023-11-03 11:37:54 +0330

    DTOs And Services Updated

--- CactusApplication/DTOs/UserDTO.cs
+++ CactusApplication/DTOs/UserDTO.cs
@@ -13,6 +13,7 @@ namespace CactusApplication.DTOs
         public string UserId { get; set; }
         public string  Email { get; set; }
         public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
         public int TotalCost { get; set; }
         public string FullAddress { get; set; }
         public string Postal { get; set; }
